a385fac47a43bc244c6b35ed4e412310cabc209a
Nolan Tenpas <ntenpasdeveloper@gmail.com>
2016-06-26 14:19:34 -0500

    passport local mongoose working - i think

--- server.js
+++ server.js
@@ -24,38 +24,12 @@ app.use(passport.session());
 // connect to db and build schema
 var db = mongoose.connection;
 var User; // model
-var Account; // schema
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function() {
-  Account = new mongoose.Schema({
-    username: String,
-    password: String
-  });
-  User = mongoose.model('User', Account);
-
-  Account.plugin(passportLocalMongoose);
-  Account.validPassword = function(username, password) {
-    User.findOne({username: username, password: password}, function(err, user) {
-      if (user)
-        return true;
-      return false;
-    });
-  }
-  Account.authenticate = function(username, password, done) {
-    User.findOne({username: username}, function(err, user) {
-      if (err)
-        return done(err);
-      if (!user)
-        return done(null, false, {message: 'Incorrect Username'});
-      if (!user.validPassword(username, password)) {
-        return done(null, false, {message: 'Incorrect Password'});
-      return done(null, user);
-      }
-    });
-  }
-  passport.use(new LocalStrategy(Account.authenticate()));
-  passport.serializeUser(Account.serializeUser());
-  passport.deserializeUser(Account.deserializeUser());
+  User = require('./user');
+  passport.use(new LocalStrategy(User.authenticate()));
+  passport.serializeUser(User.serializeUser());
+  passport.deserializeUser(User.deserializeUser());
 });
 
 
@@ -68,19 +42,17 @@ app.post('/signup', data.array(), function(req, res) {
   var username = req.body.username;
   var password = req.body.password;
 
-  Account.register(new Account({username: username}), password,
-    function(err, account) {
+  User.register(new User({username: username}), password,
+    function(err) {
       if (err)
         res.redirect('/error');
-        //return res.render('signup', {account: account});
-      passport.authenticate('local')(req, res, function() {
-        res.redirect('/profile')
-      });
+      console.log('user registered');
+      res.redirect('/profile');
     });
 });
 
-app.post('/login', passport.authenticate('local'), function(req, res) {
-  res.redirect('/');
+app.post('/login', data.array(), passport.authenticate('local'), function(req, res) {
+  res.redirect('/profile');
 });
 
 app.get('/profile', data.array(), function(req, res) {
